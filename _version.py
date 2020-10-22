56a8646e4e1791a0edad2aa8dd9e25e90cc220f9
jared <jaredjulien@exsystems.net>
2020-10-22 17:02:53 -0400

    bumped patch version

--- _version.py
+++ _version.py
@@ -19,6 +19,6 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ----------------------------------------------------------------------------------------------------------------------
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # End of File
